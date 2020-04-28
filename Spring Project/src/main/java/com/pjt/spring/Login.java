ec4adef5a3662b03550f684f111a45d126f21503
Juwon <seojw0830@gmail.com>
2020-04-28 21:51:31 +0900

    200428 - STS를 이용한  스프링 MVC 웹서비스 2탄

---
+++ Spring Project/src/main/java/com/pjt/spring/Login.java
@@ -0,0 +1,17 @@
+package com.pjt.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Login {
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		model.addAttribute("loginKey", "loginValue");
+		
+		return "login"; // login.jsp
+	}
+}
