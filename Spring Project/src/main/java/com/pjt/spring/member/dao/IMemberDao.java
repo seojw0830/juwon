571ec5a7f5d4035fc87332bb64dbc01d49b8baa3
Juwon <seojw0830@gmail.com>
2020-04-30 21:40:34 +0900

    200430 - service & dao 구현

---
+++ Spring Project/src/main/java/com/pjt/spring/member/dao/IMemberDao.java
@@ -0,0 +1,10 @@
+package com.pjt.spring.member.dao;
+
+import com.pjt.spring.member.Member;
+
+public interface IMemberDao {
+	void memberInsert(String memId, String memPw, String memMail, String memPhone1, String memPhone2, String memPhone3);
+	Member memberSelect(String memId, String memPw);
+	void memberUpdate();
+	void memberDelete();
+}
---
+++ Spring Project/src/main/java/com/pjt/spring/member/service/IMemberService.java
@@ -0,0 +1,10 @@
+package com.pjt.spring.member.service;
+
+import com.pjt.spring.member.Member;
+
+public interface IMemberService {
+	void memberRegister(String memId, String memPw, String memMail, String memPhone1, String memPhone2, String memPhone3);
+	Member memberSearch(String memId, String memPw);
+	void memberModify();
+	void memberRemove();
+}
