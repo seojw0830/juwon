bd8a86d1d4fc567d17eee224ae0a0f6b908402ef
Juwon <seojw0830@gmail.com>
2020-05-03 19:43:43 +0900

    200503 - Controller 객체 구현

--- Spring Project/src/main/java/com/pjt/spring/member/controller/MemberController.java
+++ Spring Project/src/main/java/com/pjt/spring/member/controller/MemberController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pjt.spring.member.Member;
 import com.pjt.spring.member.service.MemberService;
 
 @Controller
+//@RequestMapping("/member") 경로를 묶어줌
 public class MemberController {
 
 //	MemberService service = new MemberService();
@@ -19,7 +21,7 @@ public class MemberController {
 	@Resource(name="memService")
 	MemberService service;
 	
-	@RequestMapping(value="/memJoin", method=RequestMethod.POST)
+	@RequestMapping(value="/memJoin", method=RequestMethod.POST) // 폼에서 넘어오는 방식이 POST인 경우, GET방식은 똑같이 GET으로 써주던가 안써줘도된다.
 	public String memJoin(Model model, HttpServletRequest request) {
 		String memId = request.getParameter("memId");
 		String memPw = request.getParameter("memPw");
@@ -39,10 +41,10 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value="/memLogin", method=RequestMethod.POST)
-	public String memLogin(Model model, HttpServletRequest request) {
-		
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
+//	public String memLogin(Model model, HttpServletRequest request) {
+	public String memLogin(Model model, @RequestParam("memId") String memId, @RequestParam("memPw") String memPw)	{
+//		String memId = request.getParameter("memId");
+//		String memPw = request.getParameter("memPw");
 		
 		Member member = service.memberSearch(memId, memPw);
 		
